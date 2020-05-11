ce0f687cd248a7d9a2751916b12fdeaac32f7488
Sean Tay <sean.tay@shopee.com>
2020-05-11 16:17:35 +0800

    :heavy_plus_sign: Added @babel/plugin-transform-runtime and @babel/runtime to fix async Arrow Function Error.

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -63,6 +63,10 @@ const baseConfig = {
                             presets: [
                                 '@babel/preset-env',
                                 '@babel/preset-typescript'
+                            ],
+
+                            plugins: [
+                                '@babel/plugin-transform-runtime',
                             ]
                         }
                     },

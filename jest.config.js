46cf986151e3200d9121d1af09e4aa58a5419e36
Sean-Tay <taysiangmengsean@gmail.com>
2020-07-26 18:23:54 +0800

    :art: Change comment.

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(PATHS.PATH_PROJECT_ROOT, './__mocks__'),
         '\\.(s?css|less)$': 'identity-obj-proxy',
 
-        // Project Alias
+        // Aliases are used to shorten import or require declarations in Files.
         '^@/(.*)$': path.resolve(PATHS.PATH_SRC, './$1'),
     }
 };
